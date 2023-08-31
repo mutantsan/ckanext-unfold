81fbb12e9fcd3ea0dc2aec0f8203330f22ed189d
Oleksandr Cherniavskyi <kvaqich@gmail.com>
2023-08-31 18:20:45 +0300

    feature: implement search by archive tree

--- ckanext/unfold/assets/unfold-init-jstree.js
+++ ckanext/unfold/assets/unfold-init-jstree.js
@@ -6,6 +6,13 @@ ckan.module("unfold-init-jstree", function ($, _) {
         },
 
         initialize: function () {
+            $.proxyAll(this, /_on/);
+
+            this.tree = $(this.el)
+
+            $("#jstree-search").on("change", this._onSearch);
+            $("#jstree-search-clear").click(this._onClearSearch);
+
             $(this.el).jstree({
                 'core': {
                     'data': this.options.data
@@ -15,5 +22,13 @@ ckan.module("unfold-init-jstree", function ($, _) {
                 ]
             });
         },
+
+        _onSearch: function (e) {
+            this.tree.jstree("search", $(e.target).val())
+        },
+
+        _onClearSearch: function (e) {
+            $("#jstree-search").val("").trigger("change");
+        }
     };
 });
